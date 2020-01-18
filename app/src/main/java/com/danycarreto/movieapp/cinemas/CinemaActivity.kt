70fa54ef750b19a6a8b7287f3330337701e1ff88
Daniel Castillo Carreto <dany_carreto@hotmail.com>
2020-01-18 10:34:20 -0600

    Added event map

--- app/src/main/java/com/danycarreto/movieapp/cinemas/CinemaActivity.kt
+++ app/src/main/java/com/danycarreto/movieapp/cinemas/CinemaActivity.kt
@@ -1,11 +1,14 @@
 package com.danycarreto.movieapp.cinemas
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.danycarreto.movieapp.R
@@ -15,11 +18,24 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_cinema.*
 
 class CinemaActivity : AppCompatActivity(), OnMapReadyCallback,
-    GoogleApiClient.ConnectionCallbacks, LocationListener {
+    GoogleApiClient.ConnectionCallbacks, LocationListener,
+    GoogleMap.OnMarkerClickListener {
+
+    override fun onMarkerClick(marker: Marker?): Boolean {
+        println(marker?.position?.latitude)
+        println(marker?.position?.longitude)
+        txtTitle.text = marker?.title
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+        return true
+    }
 
 
     private lateinit var fragmentMap: SupportMapFragment
@@ -30,6 +46,7 @@ class CinemaActivity : AppCompatActivity(), OnMapReadyCallback,
     private lateinit var googleApiClient: GoogleApiClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<View>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,6 +72,10 @@ class CinemaActivity : AppCompatActivity(), OnMapReadyCallback,
                 }
             }
         }
+
+        //BottomSheet
+        bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
     }
 
     fun setNewLocation(location:Location){
@@ -75,6 +96,49 @@ class CinemaActivity : AppCompatActivity(), OnMapReadyCallback,
 
     override fun onMapReady(map: GoogleMap) {
         googleMap = map
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        //GoogleMap.MAP_TYPE_NORMAL
+        //GoogleMap.MAP_TYPE_SATELLITE
+        //GoogleMap.MAP_TYPE_TERRAIN
+        //GoogleMap.MAP_TYPE_HYBRID
+        //googleMap.uiSettings.setAllGesturesEnabled(false)
+        googleMap.setOnMarkerClickListener(this)
+        googleMap.setOnMapClickListener {
+            try {
+                val i = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(
+                        "geo:<${it?.latitude}>,<${it?.longitude}>?q=" +
+                                "<${it?.latitude}>,<${it.longitude}>(${getString(R.string.app_name)})"
+                    )
+                )
+                i.setClassName(
+                    "com.google.android.apps.maps",
+                    "com.google.android.maps.MapsActivity"
+                )
+                startActivity(i)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        googleMap.setOnMarkerDragListener(object : GoogleMap.OnMarkerDragListener {
+            override fun onMarkerDragEnd(marker: Marker?) {
+                println("DragEndLatitude:  ${marker?.position?.latitude}")
+                println("DragEndLongitude:  ${marker?.position?.longitude}")
+            }
+
+            override fun onMarkerDragStart(marker: Marker?) {
+                println("DragStartLatitude:  ${marker?.position?.latitude}")
+                println("DragStartLongitude:  ${marker?.position?.longitude}")
+            }
+
+            override fun onMarkerDrag(marker: Marker?) {
+                println("DragLatitude:  ${marker?.position?.latitude}")
+                println("DragLongitude:  ${marker?.position?.longitude}")
+            }
+
+        })
+        googleMap.isBuildingsEnabled = false
         loadCinemas()
     }
 
@@ -93,11 +157,13 @@ class CinemaActivity : AppCompatActivity(), OnMapReadyCallback,
             MarkerOptions()
                 .position(cinemexVallejo)
                 .title("Cinemex Vallejo")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
         )
         googleMap.addMarker(
             MarkerOptions()
                 .position(cinemexDelta)
                 .title("Cinemex Delta")
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.logo_login))
         )
         googleMap.moveCamera(CameraUpdateFactory
             .newLatLngZoom(cinemexDelta, 12f))
